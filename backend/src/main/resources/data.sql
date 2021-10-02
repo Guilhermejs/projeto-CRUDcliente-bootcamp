031bf873aee059cd1ed3099efeaa8b1b4d831667
Guilherme Jacques <guilherme.jacques@hotmail.com>
2021-10-02 16:38:42 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Guilherme', '99999999', '3000.00', NOW(), '1');
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Nathaly', '11111111', '5000.00', NOW(), '2');
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Raquel', '222222222', '5000.00', NOW(), '2');
